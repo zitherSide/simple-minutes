031a67cd97bcb4746b07b2956c32f1e4a0b88891
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-06-07 20:04:20 +0900

    Deploy to heroku. Added dummy edit page.

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-pencil',
+          title: 'NewItem',
+          to: '/NewItem'
         }
       ],
       miniVariant: false,
---
+++ pages/NewItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>hoge</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
