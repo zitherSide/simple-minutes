125d788aaf4c1b13ae2041e28ca26625146ba40d
kotoyori.takuya <kotoyori.takuya@canon.co.jp>
2020-06-12 13:17:35 +0900

    Added Edit dialog.

---
+++ components/SelectableInput.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <v-container>
+        <v-layout column>
+            <v-card>
+                <v-card-title> {{label}} </v-card-title>
+                <v-combobox/>
+            </v-card>
+        </v-layout>
+    </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'label',
+    ],
+}
+</script>
--- pages/NewItem.vue
+++ pages/NewItem.vue
@@ -1,9 +1,46 @@
 <template>
-    <h1>hoge</h1>
+    <v-dialog
+        v-model=showEditDlg
+        width=500>
+        <v-card>
+            <v-card-title>Write new item.</v-card-title>
+            <v-divider/>
+            <v-container>
+                <v-layout row>
+                    <SelectableInput
+                        :label="Type"
+                    />
+                </v-layout>
+                <v-card-actions>
+                    <v-spacer/>
+                    <v-btn
+                        color="primary"
+                        @click="showEditDlg = false">
+                        Create
+                    </v-btn>
+                    <v-spacer/>
+                    <v-btn
+                        color="secondary"
+                        @click="showEditDlg = false">
+                        Cancel
+                    </v-btn>
+                    <v-spacer/>
+                </v-card-actions>
+            </v-container>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
+import SelectableInput from '~/components/SelectableInput.vue'
 export default {
-    
+    data(){
+        return {
+            showEditDlg : true,
+        }
+    },
+    components : {
+        SelectableInput
+    }
 }
 </script>
