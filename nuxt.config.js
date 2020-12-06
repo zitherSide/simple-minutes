2428569a1d7e7cd1765e9846d2e576f85b477879
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-12-06 10:47:49 +0900

    deleted an unused asset link.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,6 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
